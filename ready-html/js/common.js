11b24b32ddcada3844df900f4cb2874541ba522a
nika <nikaonime@gmail.com>
2022-06-23 19:05:07 +0300

    end

--- ready-html/js/common.js
+++ ready-html/js/common.js
@@ -1,6 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-	// $('body').hide()
+const showSlider = new Swiper('.showcase-carousel', {
+	loop: true,
+	slidesPerView: 3,
+	speed: 1800,
+	centeredSlides: true,
+	navigation: {
+		nextEl: ".showcase-navigation__next",
+		prevEl: ".showcase-navigation__prev"
+	}
+})
+
 document.querySelector('video').playbackRate = 2
 
 })
